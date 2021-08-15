411f6cb06a6cbb6e39cf559091ecef95e05d1aee
Mike Tate <shagee@uw.edu>
2021-08-15 07:34:14 -0700

    Added search for movie

--- movieratings/src/components/movieList.js
+++ movieratings/src/components/movieList.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import Movie from './movie';
+import React from "react";
+import Movie from "./movie";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/row";
 import Col from "react-bootstrap/col";
-import CardGroup from "react-bootstrap/cardgroup";
 
-export default function Movies({movies}) {
-
-    return (
-        <Container>
-            <CardGroup>
-            <Row xs={1} md={2} lg={3} className="g-4">
-            {
-            movies && movies.map(movie => {
-                return (<Col key={movie.title}><Movie movie={movie}></Movie></Col>)
-            })
-            }
-            </Row>
-            </CardGroup>
-        </Container>
-    )
+export default function Movies({ movies }) {
+  return (
+    <Container>
+        <Row xs={1} md={2} lg={3} className="g-4">
+          {movies &&
+            movies.map((movie) => {
+              return (
+                <Col key={movie.title}>
+                  <Movie movie={movie}></Movie>
+                </Col>
+              );
+            })}
+        </Row>
+    </Container>
+  );
 }
