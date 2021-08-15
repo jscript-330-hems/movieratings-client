432a0acd27f1629684935699d0c55bd2f3cc7a98
Mike Tate <shagee@uw.edu>
2021-08-15 07:11:12 -0700

    Modified layout of movie list

--- movieratings/src/components/movieList.js
+++ movieratings/src/components/movieList.js
@@ -3,18 +3,21 @@ import Movie from './movie';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/row";
 import Col from "react-bootstrap/col";
+import CardGroup from "react-bootstrap/cardgroup";
 
 export default function Movies({movies}) {
 
     return (
         <Container>
-            <Row>
+            <CardGroup>
+            <Row xs={1} md={2} lg={3} className="g-4">
             {
             movies && movies.map(movie => {
                 return (<Col key={movie.title}><Movie movie={movie}></Movie></Col>)
             })
             }
             </Row>
+            </CardGroup>
         </Container>
     )
 }
