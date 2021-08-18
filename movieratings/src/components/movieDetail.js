ebf735dcae2c55fff4527ab9d141cad5e6274bb9
Mike Tate <shagee@uw.edu>
2021-08-18 16:54:28 -0700

    add button for write a review and back to movie list

--- movieratings/src/components/movieDetail.js
+++ movieratings/src/components/movieDetail.js
@@ -1,11 +1,14 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
+import { useHistory, useParams } from "react-router-dom";
 import ReactStars from "react-rating-stars-component";
+import { AppContext } from "../context";
 import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
 
 export default function Movie() {
-
   const { id } = useParams();
+  const history = useHistory();
+  const { user } = useContext(AppContext);
 
   //https://www.npmjs.com/package/react-stars
   const [movie, setMovie] = useState({});
@@ -13,47 +16,62 @@ export default function Movie() {
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_SERVICE_BASE_URL}/movies/${id}`)
-        .then(res => res.json())
-        .then(data => setMovie(data))
-        .catch(err => console.error(err))
-        .finally(() => setLoading(false));
-}, [id]);
+      .then((res) => res.json())
+      .then((data) => setMovie(data))
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
+  }, [id]);
 
-    return (
-        <div>
-          {loading && <div>Still loading...</div>}
-          {!loading && !movie && <div>Done loading but no movie.  Hmm.  Weird</div>}
-          {!loading && movie && (
-            <>
-            <h1>{movie.title}</h1>
-            <img alt={movie.title} src={movie.moviePicUrl} />
-            <Table>
-              <tbody>
-                <tr>
-                  <td>Rating</td>
-                  <td><ReactStars count={10} value={movie.averageScore} edit={false} /> (avg {movie.averageScore} out of {movie.reviewCount} reviews)</td>
-                </tr>
-                <tr>
-                  <td>Actors</td>
-                  <td>{movie.actors.join(", ")}</td>
-                </tr>
-                <tr>
-                  <td>Release year</td>
-                  <td>{movie.releaseYear}</td>
-                </tr>
-                <tr>
-                  <td>Rating</td>
-                  <td>{movie.rating}</td>
-                </tr>
-                <tr>
-                  <td>Synopsis</td>
-                  <td>{movie.synopsis}</td>
-                </tr>
-              </tbody>
-            </Table>
-            </>
+  return (
+    <div>
+      {loading && <div>Still loading...</div>}
+      {!loading && !movie && <div>Done loading but no movie. Hmm. Weird</div>}
+      {!loading && movie && (
+        <>
+          <h1>{movie.title}</h1>
+          <img alt={movie.title} src={movie.moviePicUrl} />
+          <Table>
+            <tbody>
+              <tr>
+                <td>Rating</td>
+                <td>
+                  <ReactStars
+                    count={10}
+                    value={movie.averageScore}
+                    edit={false}
+                  />{" "}
+                  (avg {movie.averageScore} out of {movie.reviewCount} reviews)
+                </td>
+              </tr>
+              <tr>
+                <td>Actors</td>
+                <td>{movie.actors.join(", ")}</td>
+              </tr>
+              <tr>
+                <td>Release year</td>
+                <td>{movie.releaseYear}</td>
+              </tr>
+              <tr>
+                <td>Rating</td>
+                <td>{movie.rating}</td>
+              </tr>
+              <tr>
+                <td>Synopsis</td>
+                <td>{movie.synopsis}</td>
+              </tr>
+            </tbody>
+          </Table>
+          <Button onClick={() => history.push("/movies")}>
+            Back to movie list
+          </Button>
+          &nbsp;
+          {user && (
+            <Button onClick={() => history.push(`/writereview/${id}`)}>
+              Write a review
+            </Button>
           )}
-        </div>
-
-    )
+        </>
+      )}
+    </div>
+  );
 }
