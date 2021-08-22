1bbb8f3a266a2096599ddcdcc4f4147cfe2fa4ce
patternchef <1952676+patternchef@users.noreply.github.com>
2021-08-22 16:32:07 -0700

    Revert "Merge pull request #12 from jscript-330-hems/dev"
    This reverts commit b7ab2a0b4640b46d2fb36739a8d1c6f31a464921, reversing
    changes made to 8b6181f0dcfba644650ef91dfcea7cb6c338d5e6.

--- movieratings/src/components/movie.js
+++ movieratings/src/components/movie.js
@@ -1,34 +1,26 @@
-import { useHistory } from "react-router-dom";
 import Card from "react-bootstrap/card";
 import Button from "react-bootstrap/button";
 import ReactStars from "react-rating-stars-component";
 
-export default function Movie({ movie }) {
-  const history = useHistory();
-
-  return (
-    <div className="card mb-3" style={{ maxWidth: "540px" }}>
-      <div className="row no-gutters">
-        <div className="col-md-4">
-          <img src={movie.moviePicUrl} className="card-img" alt={movie.title} />
-        </div>
-        <div className="col-md-8">
-          <Card.Body style={{ padding: "5px" }}>
-            <Card.Title>{movie.title}</Card.Title>
-            <ReactStars count={5} value={movie.averageScore} edit={false} />
-            <Card.Text>
-              <small className="text-muted">Rated: {movie.rating}</small>
-            </Card.Text>
-            <Button
-              variant="primary"
-              size="sm"
-              onClick={() => history.push(`/moviedetail/${movie._id}`)}
-            >
-              See details
-            </Button>
-          </Card.Body>
-        </div>
-      </div>
+export default function Movie({movie}) {
+    return (
+        
+<div className="card mb-3" style={{maxWidth: "540px"}}>
+  <div className="row no-gutters">
+    <div className="col-md-4">
+      <img src="https://www.joblo.com/wp-content/uploads/2021/03/tom-jerry-poster-1-400x600.jpg" className="card-img" alt={movie.title} />
     </div>
-  );
+    <div className="col-md-8">
+    
+    <Card.Body style={{padding: "5px"}}>
+        <Card.Title>{movie.title}</Card.Title>
+        <ReactStars count={5} value={movie.averageScore} edit={false} />
+        <Card.Text><small className="text-muted">Rated: {movie.rating}</small></Card.Text>
+        <Button variant="primary" size="sm">See details</Button>
+      </Card.Body>
+    </div>
+  </div>
+</div>
+
+    )
 }
