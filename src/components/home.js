01c527c1e4e1f21ddb5433ee2dc2b413141b6b27
Sean Chao <>
2021-08-22 17:49:06 -0700

    moving files up a level

---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchForMovie from './searchForMovie';
+
+export default function Home() {
+
+
+    return (
+        <>
+        <h1>Home Page</h1>
+        <p>Welcome to our movie ratings site.</p>
+        <SearchForMovie></SearchForMovie>
+        </>
+    )
+}
---
+++ src/components/notAnAdmin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotAnAdmin() {
+    return (
+        <>
+        <h1>You aren't an admin</h1>
+        <p>Sorry.  You aren't allowed to be here.</p>
+        </>
+    )
+}
