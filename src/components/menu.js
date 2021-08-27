88ff8f129eb0dcba43ad0bad91aaa2a12590e28a
Hiatt Campbell <hiattc16@uw.edu>
2021-08-26 22:37:31 -0700

    Added write review component

--- src/components/menu.js
+++ src/components/menu.js
@@ -3,36 +3,50 @@ import { Link } from "react-router-dom";
 import LogOut from "./logout";
 import { AppContext } from "../context";
 
-export default function Home() {
+export default function Menu() {
   const { user } = useContext(AppContext);
 
   return (
     <>
-      <Link to="/">Home</Link>
-      <br />
-      <Link to="/movies">List of movies</Link>
-      <br />
-      {user && (
-        <>
-          <Link to="/writereview">Write a review</Link>
-          <br />
-        </>
-      )}
+      <nav className="navbar navbar-expand-lg bg-primary">
+        <Link className="menuLink" to="/">
+          Home
+        </Link>
+        <br />
+        <Link className="menuLink" to="/movies">
+          List of movies
+        </Link>
+        <br />
+        {user && (
+          <>
+            <Link className="menuLink" to="/writereview">
+              Write a review
+            </Link>
+            <br />
+          </>
+        )}
 
-      {!user && (
-        <>
-          <Link to="/signup">Sign up</Link>
-          <br />
-          <Link to="/login">Log in</Link>
-          <br />
-        </>
-      )}
+        {!user && (
+          <>
+            <Link className="menuLink" to="/signup">
+              Sign up
+            </Link>
+            <br />
+            <Link className="menuLink" to="/login">
+              Log in
+            </Link>
+            <br />
+          </>
+        )}
 
-      {user && user.roles.indexOf("admin") > -1 && (
-        <Link to="/adminarea">Admin Area</Link>
-      )}
-      <br />
-      {user && <LogOut />}
+        {user && user.roles.indexOf("admin") > -1 && (
+          <Link className="menuLink" to="/adminarea">
+            Admin Area
+          </Link>
+        )}
+        <br />
+        {user && <LogOut />}
+      </nav>
     </>
   );
 }
